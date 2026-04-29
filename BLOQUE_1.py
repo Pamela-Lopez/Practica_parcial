4aeb9a3a620879212a611af6abd9c2b9637c89ab
Pamela Lopez <pamela05.lop@gmail.com>
2026-04-28 21:37:00 -0300

    Resolucion ejercicio 1.2

--- BLOQUE_1.py
+++ BLOQUE_1.py
@@ -1,5 +1,6 @@
 import math
 
+#Ejercicio 1.1
 #Se crea una clase circulo:
 class Circulo:
     def __init__(self,radio):    #Se inicializa la clase con atributos
@@ -22,3 +23,34 @@ circ2=Circulo(3)
 print(circ2.area())
 print(circ1.perimetro())
 print(circ2.diametro())
+
+#Ejercicio 1.2
+# Se crea la clase cuenta bancaria
+
+class CuentaBancaria:
+
+    def __init__(self,titular,saldo):
+        self.titular = titular
+        self._saldo = saldo    #Se usa el pilar de POO encapsulamiento
+
+    def depositar(self,monto):
+        self._saldo += monto
+        return f"Se deposito a la cuenta: ${monto}.Total saldo:$ {self._saldo}"
+
+    def extraer(self, monto):
+        if monto > self._saldo:
+            return "Saldo insuficiente,ingrese otro monto"
+        else:
+             self._saldo - monto
+             return f"¡Operacion exitosa!. Su saldo actual es: ${self._saldo}"
+    
+    def consultar_saldo(self):
+        return f"Su saldo actual es de: ${self._saldo}"
+    
+cuenta1=CuentaBancaria("Juan Perez",1000)
+cuenta2=CuentaBancaria("Juana Paz", 50000)
+
+print(cuenta1.titular)
+print(cuenta2.depositar(10000))
+print(cuenta1.extraer(2000))
+print(cuenta2.consultar_saldo())
